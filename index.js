9823ed169c963f4c28d8d18bab75b3a81ef84f9e
Bindu Prasanthi <bindu.karpuram@gmail.com>
2023-07-25 06:23:14 +0000

    remove test line

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const addDays = require("date-fns/addDays");
 
 const getDateAfterXDays = (days) => {
   const result = addDays(new Date(2020, 7, 22), days);
-  const a = 1;
   return "${result.getDate()}-${result.getMonth()+1}-${result.getFullYear()}";
 };
 module.exports = getDateAfterXDays;
